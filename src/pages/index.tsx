0cde8304af8f7923f97a943a3bfb42aa56d68727
Mehrab <mehrab572@gmail.com>
2024-04-20 20:28:58 +0330

    aha

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import GithubSvg from '../assets/GithubSvg';
 
 const Home: NextPage = () => {
-  console.log('window.Telegram', window.Telegram);
+  console.log('window.Telegram');
   console.log('window.WebApp', window.Telegram.WebApp);
 
   return (
@@ -17,6 +17,9 @@ const Home: NextPage = () => {
       <main className="flex w-full flex-1 flex-col items-center justify-center px-4 text-center">
         <span className="text-2xl font-bold text-telegram-black">HI</span>
       </main>
+      <span>{JSON.stringify(window.Telegram)}</span>
+      <span>===========================</span>
+      <span>{JSON.stringify(window.Telegram.WebApp)}</span>
 
       <footer className="flex h-20 w-full items-center justify-center border-t border-t-telegram-black">
         FOOTER
